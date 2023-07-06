2916b35920abc6801b7be7edec3050a342f56a38
Shadowed229 <83278462+Shadowed229@users.noreply.github.com>
2023-07-06 20:58:10 +0800

    Conveyor belt fixed (but only 1 item can spawn)

--- Assets/Scripts/RubbishConveyor.cs
+++ Assets/Scripts/RubbishConveyor.cs
@@ -5,21 +5,67 @@ using UnityEngine;
 public class RubbishConveyor : MonoBehaviour
 {
 
-    public Transform checkpoint;
+    public Transform rubbish;
+    //public Transform checkPoint;
     private float speed = 1;
+    private bool occupied;
+    public GameObject trashBag;
+    public Transform spawner;
+    private float offset;
+    
     //public Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
+        spawner = GameObject.FindGameObjectWithTag("Spawner").transform;
+        Instantiate(trashBag, spawner);
+        occupied = false;
+        //rubbish = GameObject.FindGameObjectWithTag("Rubbish").transform;
+        //checkPoint = GameObject.FindGameObjectWithTag("checkpoint").transform;
+        offset = 0.2f;
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 target = new Vector3(checkpoint.transform.position.x, checkpoint.transform.position.y, checkpoint.transform.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        //rubbish = GameObject.FindGameObjectWithTag("Rubbish").transform;
+        if (occupied == false)
+        {
+            Vector3 target = new Vector3(transform.position.x, transform.position.y + offset , transform.position.z);
+            rubbish = GameObject.FindGameObjectWithTag("Rubbish").transform;
+            rubbish.transform.position = Vector3.MoveTowards(rubbish.transform.position, target, Time.deltaTime * speed);
+        }
+            
+        
+        
+    }
+    private void FixedUpdate()
+    {
+        rubbish = GameObject.FindGameObjectWithTag("Rubbish").transform;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Rubbish"))
+        {
+            Debug.Log("Rubbish has Entered!");
+            occupied = true;
+        }
+        
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Rubbish"))
+        {
+            occupied = false;
+
+            Instantiate(trashBag, spawner);
+            Debug.Log("Rubbish has spawned!");
+            
+        }
+
     }
 
-    
+
 }
