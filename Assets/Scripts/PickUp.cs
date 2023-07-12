75022b0491e6a8b2db823f95ad055ee8717c6eb9
Shadowed04 <2100340E@student.tp.edu.sg>
2023-07-12 15:43:26 +0800

    drop function done, able to open up rubbish bags and random things will spawn

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    public Transform rubbishpos;
-    public GameObject trashBag;
-    public GameObject rubbish;
+    private Transform rubbishpos;
+    private Transform plasticpos;
+    public GameObject trashBagPrefab;
+    private GameObject rubbish;
+    private GameObject plastic;
+    public GameObject plasticPrefab;
     public Transform ItemHolder;
-    private bool holding;
+    public static bool holding;
+    public static bool plastichold;
+    public static bool trashhold;
     // Start is called before the first frame update
     void Start()
     {
         holding = false;
+        plastichold = false;
+        trashhold = false;
     }
     private void FixedUpdate()
     {
         rubbishpos = GameObject.FindGameObjectWithTag("Rubbish").transform;
         rubbish = GameObject.FindGameObjectWithTag("Rubbish");
+        plastic = GameObject.FindGameObjectWithTag("Plastic");
+        plasticpos = GameObject.FindGameObjectWithTag("Plastic").transform;
     }
 
     // Update is called once per frame
@@ -25,17 +34,32 @@ public class PickUp : MonoBehaviour
     {
         if (rubbish != null)
         {
-            float distancebetween = Vector3.Distance(transform.position, rubbish.transform.position);
-            if (distancebetween < 3)
+            float distancebetweenRubbish = Vector3.Distance(transform.position, rubbish.transform.position);
+            float distancebetweenPlastic = Vector3.Distance(transform.position, plastic.transform.position);
+            if (distancebetweenRubbish < 2)
             {
-                if (Input.GetButtonDown("Pickup") && holding == false)
+                if (Input.GetButtonDown("Pickup") && holding == false )
                 {
                     Vector3 target = new Vector3(100, 0, 0);
                     rubbishpos.transform.position = target;
                     RubbishConveyor.maxRubbish -= 1;
                     Destroy(rubbish);
-                    Instantiate(trashBag, ItemHolder);
+                    Instantiate(trashBagPrefab, ItemHolder);
                     holding = true;
+                    trashhold = true;
+                }
+            }
+            if (distancebetweenPlastic < 2)
+            {
+                if (Input.GetButtonDown("Pickup") && holding == false)
+                {
+                    Vector3 target = new Vector3(100, 0, 0);
+                    plasticpos.transform.position = target;
+                    RubbishConveyor.maxRubbish -= 1;
+                    Destroy(plastic);
+                    Instantiate(plasticPrefab, ItemHolder);
+                    holding = true;
+                    plastichold = true;
                 }
             }
         }
---
+++ Assets/Scripts/WorkStation.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkStation : MonoBehaviour
+{
+    private GameObject trash;
+    public GameObject[] allTrash;
+
+    public Transform placeholder;
+    public Transform placeholder2;
+    public Transform placeholder3;
+    public static bool occupied;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PickUp.holding == true && occupied == false && PickUp.trashhold == true)
+        {
+            if (Input.GetButtonDown("Pickup"))
+            {
+                PickUp.trashhold = false;
+                trash = GameObject.FindGameObjectWithTag("Trash");
+                Destroy(trash);
+                PickUp.holding = false;
+                int whichrubbish = Random.Range(0, 2);
+                int rubbishspawn = Random.Range(0, 3);
+                if (rubbishspawn == 0) //1 rubbish spawn
+                {
+                    GameObject spawnable = allTrash[whichrubbish];
+                    Instantiate(spawnable,placeholder);
+                    whichrubbish = Random.Range(0, 2);
+                }
+                else if (rubbishspawn == 1) //2 rubbish spawn
+                {
+                    GameObject spawnable = allTrash[whichrubbish];
+                    Instantiate(spawnable, placeholder);
+                    whichrubbish = Random.Range(0, 2);
+                    GameObject spawnable2 = allTrash[whichrubbish];
+                    Instantiate(spawnable2, placeholder2);
+                }
+                else if (rubbishspawn == 2) //3 rubbish spawn
+                {
+                    GameObject spawnable = allTrash[whichrubbish];
+                    Instantiate(spawnable, placeholder);
+                    whichrubbish = Random.Range(0, 2);
+                    GameObject spawnable2 = allTrash[whichrubbish];
+                    Instantiate(spawnable2, placeholder2);
+                    whichrubbish = Random.Range(0, 2);
+                    GameObject spawnable3 = allTrash[whichrubbish];
+                    Instantiate(spawnable3, placeholder3);
+                }
+
+
+            }
+        }
+    }
+}
