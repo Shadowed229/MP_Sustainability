6ab2bd05613a0ac86f97b49417ca0856483e41b4
Shadowed04 <2100340E@student.tp.edu.sg>
2023-07-27 13:41:34 +0800

    fixed some UI elements and Added a Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class Timer : MonoBehaviour
         {
             Debug.Log("GAME OVER!!!");
             LevelManager.instance.isGameOver = true;
+            SceneManager.LoadScene("GameOver");
         }
         DisplayTime(timeValue);
     }
@@ -32,6 +34,6 @@ public class Timer : MonoBehaviour
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
-        timeText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
