1b911100905ccb5bc06216bf0e3bd81a8eea3c22
Jungyupkim <jungyupkim1212@gmail.com>
2023-08-07 10:12:26 +0800

    test

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i<popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[popUpIndex].SetActive(true);
+            }
+            else
+            {
+                popUps[popUpIndex].SetActive(false);
+            }
+        }
+    }
+}
