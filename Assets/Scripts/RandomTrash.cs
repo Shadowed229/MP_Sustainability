25efc61c22cc9ffb8b7fd473fd3d79acb0403bf9
Shadowed04 <2100340E@student.tp.edu.sg>
2023-08-22 12:10:28 +0800

    Duplicate pls

--- Assets/Scripts/RandomTrash.cs
+++ Assets/Scripts/RandomTrash.cs
@@ -47,43 +47,54 @@ public class RandomTrash : MonoBehaviour
             //rubbishspawn = Random.Range(5, 6);
             if (LevelManager.instance.rubbishspawn == 5) //5 rubbish spawn
             {
-                whichrubbish = Random.Range(0, 2);
-                GameObject spawnable = allTrash[whichrubbish];
+                whichrubbish = Random.Range(0, 1);
+                GameObject spawnable = allTrash[whichrubbish]; //either spawns dirty can or dirty metal can
                 Instantiate(spawnable, spawn5);
-                whichrubbish = Random.Range(0, 2);
-                GameObject spawnable2 = allTrash[whichrubbish];
+
+                whichrubbish = 2;
+                GameObject spawnable2 = allTrash[whichrubbish]; // spawns dirty plastic bottle
                 Instantiate(spawnable2, spawn2);
-                whichrubbish = Random.Range(0, 7);
-                GameObject spawnable3 = allTrash[whichrubbish];
+
+                whichrubbish = Random.Range(5, 7); 
+                GameObject spawnable3 = allTrash[whichrubbish]; // Either chips, toothbrush or plastic bag
                 Instantiate(spawnable3, spawn8);
-                whichrubbish = Random.Range(0, 7);
-                GameObject spawnable4 = allTrash[whichrubbish];
+
+                whichrubbish = Random.Range(3, 4);
+                GameObject spawnable4 = allTrash[whichrubbish]; // spawns random glass item
                 Instantiate(spawnable4, spawn1);
-                whichrubbish = Random.Range(0, 7);
-                GameObject spawnable5 = allTrash[whichrubbish];
+
+                whichrubbish = 8;
+                GameObject spawnable5 = allTrash[whichrubbish]; // spawns 1 trash bag
                 Instantiate(spawnable5, spawn3);
+
                 Debug.Log("Spawning 5 Rubbish");
             }
             if (LevelManager.instance.rubbishspawn == 6) //6 rubbish spawn
             {
-                whichrubbish = Random.Range(0, 2);
-                GameObject spawnable = allTrash[whichrubbish];
+                whichrubbish = Random.Range(0, 1);
+                GameObject spawnable = allTrash[whichrubbish]; //either spawns dirty can or dirty metal can
                 Instantiate(spawnable, spawn5);
-                whichrubbish = Random.Range(0, 2);
-                GameObject spawnable2 = allTrash[whichrubbish];
+
+                whichrubbish = 2;
+                GameObject spawnable2 = allTrash[whichrubbish]; // spawns dirty plastic bottle
                 Instantiate(spawnable2, spawn2);
-                whichrubbish = Random.Range(0, 7);
-                GameObject spawnable3 = allTrash[whichrubbish];
+
+                whichrubbish = Random.Range(5, 7);
+                GameObject spawnable3 = allTrash[whichrubbish]; // Either chips, toothbrush or plastic bag
                 Instantiate(spawnable3, spawn8);
-                whichrubbish = Random.Range(0, 7);
-                GameObject spawnable4 = allTrash[whichrubbish];
+
+                whichrubbish = Random.Range(3, 4);
+                GameObject spawnable4 = allTrash[whichrubbish]; // spawns random glass item
                 Instantiate(spawnable4, spawn1);
-                whichrubbish = Random.Range(0, 7);
-                GameObject spawnable5 = allTrash[whichrubbish];
+
+                whichrubbish = 8;
+                GameObject spawnable5 = allTrash[whichrubbish]; // spawns 1 trash bag
                 Instantiate(spawnable5, spawn3);
-                whichrubbish = Random.Range(0, 7);
-                GameObject spawnable6 = allTrash[whichrubbish];
-                Instantiate(spawnable6, spawn2);
+
+                whichrubbish = Random.Range(0, 8);
+                GameObject spawnable6 = allTrash[whichrubbish]; // random rubbish
+                Instantiate(spawnable6, spawn7);
+
                 Debug.Log("Spawning 6 Rubbish");
             }
         }
