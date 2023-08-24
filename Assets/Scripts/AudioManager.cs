70ee34aac63c2bc6251a7d6fafbe48ce7043da84
Shadowed04 <2100340E@student.tp.edu.sg>
2023-08-24 14:18:47 +0800

    Working sound manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,21 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioSource audioSource;
-    private void Update()
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+    private void Start()
     {
-        if (!audioSource.isPlaying && LevelManager.instance.isPaused == false && LevelManager.instance.isGameOver == false)
+        if (PlayerPrefs.HasKey("musicVolume"))
         {
-            audioSource.Play();
-            
+            LoadVolume();
         }
-        if(LevelManager.instance.isPaused == true)
+        if (PlayerPrefs.HasKey("sfxVolume"))
         {
-            audioSource.Stop();
+            LoadSFXVolume();
         }
+        SetMusicVolume();
+        SetSFXVolume();
     }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void SetSFXVolume()
+    {
+        float sfx_vol = sfxSlider.value;
+        myMixer.SetFloat("Sfx", Mathf.Log10(sfx_vol) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", sfx_vol);
+    }
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SetMusicVolume();
+    }
+    private void LoadSFXVolume()
+    {
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetSFXVolume();
+    }
+
 
 }
